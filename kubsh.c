fb5d602180373c8571be1e38dd39d4e3ea3c489f
yakini10 <temeaissataditeyakine@gmail.com>
2025-09-20 13:55:17 +0300

    Update kubsh.c

--- kubsh.c
+++ kubsh.c
@@ -10,3 +10,34 @@ int main(){
 
    return 0;
 }
+
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+   char input[100];
+
+while(1){  
+   printf("$");
+ if(fgets(input, 100, stdin)==NULL){
+   printf("\nSortir par Ctrl+D\n");
+   break;
+}
+   input[strlen(input) - 1] = '\0';
+   
+  if(strcmp(input, "exit")==0){
+     printf("Sortir du programme\n");
+break;} 
+   if(strcmp(input, "\\q")==0){
+     printf("Sortir du programme par \\q\n");
+  printf("%s: command not found\n", input);
+if(strcmp(input, "echo")==0){
+     printf("123\n");
+}
+   return 0;
+}
+
+

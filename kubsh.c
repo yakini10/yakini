1f0cf4c1d42da88b31c00fab7c51daa098d74d33
yakini10 <temeaissataditeyakine@gmail.com>
2025-10-03 21:10:24 +0300

    Update kubsh.c

--- kubsh.c
+++ kubsh.c
@@ -2,22 +2,6 @@
 #include <string.h>
 
 int main(){
-   printf("$");
-   char input[100];
-   input[strlen(input) - 1] = '\0';
-   fgets(input, 100, stdin);
-   printf("%s: command not found\n", input);
-
-   return 0;
-}
-
-
-
-
-#include <stdio.h>
-#include <string.h>
-
-int main(){
    char input[100];
 
 while(1){  

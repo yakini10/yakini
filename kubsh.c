35a2822c13a260b6c21e7028afa3e3886ea83477
yakini10 <temeaissataditeyakine@gmail.com>
2025-10-18 14:00:58 +0300

    Update kubsh.c

--- kubsh.c
+++ kubsh.c
@@ -148,6 +148,122 @@ continue;
 
      return 0;
 }
+Code pour l'exo 8 doit avoir des correction faite
+   #include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<string.h>
+#include<signal.h>
+#include<unistd.h>
+#include<sys/wait.h>
+
+#include<readline/readline.h>
+#include<readline/history.h>
+#define HISTORY_FILE  ".kubsh_history"
+sig_atomic_t signal_received = 0;
+
+void debug(char *line){
+printf("%s\n",line);
+}
+void sig_handler(int signum){
+signal_received = signum;
+printf("Configuration reloaded");
+}
+
+void print_env_var(const char *var_name){
+if(var_name == NULL || strlen(var_name)==0){
+printf("Usage:  \\e $VARNAME\n");
+return;
+}
+if(var_name[0]=='$')
+var_name++;
+
+const char *value = getenv(var_name);
+if(value==NULL){
+printf("Variable'%s'not found.\n",var_name);
+return;
+}
+char*copy=strdup(value);
+if(!copy){
+perror("strdup");
+return;
+}
+printf("%s =\n",var_name);
+if(strchr(copy,':')){
+
+char *token = strtok(copy,":");
+while(token){
+printf("-%s\n",token);
+token= strtok(NULL,":");
+}
+} else {printf("%s\n",copy);
+}
+free (copy);
+}
+void excute_command(char input){
+char*args[11];
+int i=0;
+char * token = strtok(input, " ");
+while(token!=NULL && i<10){
+args[i++]=token;
+token = strtok(NULL, " ");
+}
+args[i]=NULL;
+if(args[0]==NULL)
+return;
+pid_t pid = fork();
+if(pid==0){
+execvp(args[0],args);
+exit(EXIT_FAILURE);
+} else if (pid>0) {
+int status;
+waitpid(pid,&status,0);
+} else {
+perror("fork");
+}
+}
+int main()
+{
+signal(SIGSTOP, sig_handler);
+read_history(HISTORY_FILE);
+
+printf("Kubsh started.\n ");
+   char *input;
+
+   while(1){
+      input=readline("$ ");
+if(signal_received) {
+signal_received=0;
+continue;
+}
+
+      if(input == NULL) {
+
+      break;
+      }
+      add_history(input);
+
+      if(!strcmp(input, "\\q")){
+         break;
+ }
+      add_history(input);
+
+      if(!strcmp(input, "\\q")){
+         break;
+      } else if(strncmp(input, "debug ",5)== 0){
+         debug(input);
+      } else if (strncmp(input,"\\e $",4)==0){
+        print_env_var(input+3);
+      } else {
+         excute_command(input);
+      }
+      free(input);
+      }
+
+   write_history(HISTORY_FILE);
+
+     return 0;
+}
 
 
 
